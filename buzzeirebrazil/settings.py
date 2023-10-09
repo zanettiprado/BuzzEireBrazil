f91e3ddee64b8f2c97dc7e3d2eda0f5e9fbb522f
zanettiprado <zanettiprado@gmail.com>
2023-10-09 19:44:17 +0000

    add new box to post details and style

--- buzzeirebrazil/settings.py
+++ buzzeirebrazil/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'django_summernote',
+    'crispy_forms',
     'blog',
 ]
 
@@ -58,6 +59,8 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRISPU_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
